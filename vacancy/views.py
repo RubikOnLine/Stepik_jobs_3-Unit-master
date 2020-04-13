811530c91d7008b3d28104c1e6dc6dbd0062e055
RubikOnLine <rrt@mail.ru>
2020-04-13 13:32:55 +0300

    Окончателный релиз проекта 3 недели

--- vacancy/views.py
+++ vacancy/views.py
@@ -11,9 +11,16 @@ class MainView(View):
         specialities = Speciality.objects.all()[:8]
         companies = Company.objects.all()[:8]
 
+        # Для подсчета количества вакансий по той или иной специальности, и количества вакансий той или иной компании составляем словари следующего формата: count_dict_spec = {'frontend': количество} и count_dict_comp = {'назв. компании': количество}
+
+        count_dict_spec = {spec.code: len(spec.vacancies.all()) for spec in specialities}
+        count_dict_comp = {comp.name: len(comp.company_vacancies.all()) for comp in companies}
+
         context = {
                    'specialities': specialities,
                    'companies': companies,
+                   'count_dict_spec': count_dict_spec,
+                   'count_dict_comp': count_dict_comp
                    }
 
         return render(request, 'vacancy/index.html', context)
@@ -36,10 +43,10 @@ class SpecialityView(View):
     def get(self, request, code:str):
 
         response_data = Speciality.objects.get(code=code)
-        vacancies1 = response_data.vacancies.all()
+        vacancies = response_data.vacancies.all()
 
         context = {
-                   'vacancies': vacancies1,
+                   'vacancies': vacancies,
                    'count': len(response_data.vacancies.all()),
                    'name': code
                    }
@@ -52,10 +59,10 @@ class CompanyView(View):
     def get(self, request, name:str):
 
         response_data = Company.objects.get(name=name)
-        vacancies2 = response_data.company_vacancies.all()
+        vacancies = response_data.company_vacancies.all()
 
         context = {
-                    'vacancies': vacancies2,
+                    'vacancies': vacancies,
                     'count': len(response_data.company_vacancies.all()),
                     'name': name
                    }
@@ -68,22 +75,6 @@ class SpecialitySingleVacancy(View):
         vacancies = Vacancy.objects.get(id=id)
         employee_c = vacancies.company.employee_count
         location = vacancies.company.location
-
-        # title = vacancies.title
-        # skills = vacancies.skills
-        # description = vacancies.description
-        # salary_min = vacancies.salary_min
-        # salary_max = vacancies.salary_max
-        # published_at = vacancies.published_at
-
-        # context = { 'vacancies': vacancies
-                    # 'title': title,
-                    # 'skills': skills,
-                    # 'description': description,
-                    # 'salary_min': salary_min,
-                    # 'salary_max': salary_max,
-                    # 'published_at': published_at
-                  # }
 
 
         return render(request, 'vacancy/speciality_single_vacansy.html', {'vacancies': vacancies, 'count': employee_c, 'loc': location})
